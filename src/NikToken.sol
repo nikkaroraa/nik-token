74589dc93a19099cf9b7b97266348f50b95d8f65
nikhil arora <narora200@gmail.com>
2022-09-10 13:56:06 +0530

    feat: ability to burn tokens

--- src/NikToken.sol
+++ src/NikToken.sol
@@ -111,6 +111,10 @@ contract NikToken {
         _mint(to, amount);
     }
 
+    function burn(uint256 amount) public onlyOwner {
+        _burn(msg.sender, amount);
+    }
+
     /* utils */
     function _transfer(
         address from,
@@ -164,5 +168,17 @@ contract NikToken {
         _totalSupply += amount;
         _balances[account] += amount;
         emit Transfer(address(0), account, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: burn from the zero address");
+
+        uint256 accountBalance = _balances[account];
+        require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
+
+        _balances[account] = accountBalance - amount;
+        _totalSupply -= amount;
+
+        emit Transfer(account, address(0), amount);
     }
 }
